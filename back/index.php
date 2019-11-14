5f7431c809c72b622a5aec3c287225cd3db23ffe
hippo <picardhippolyte@yahoo.fr>
2019-11-14 21:18:21 +0100

    CCP-17-feat/back(index.php):create index.php class

--- back/index.php
+++ back/index.php
@@ -1,3 +1,19 @@
 <?php
-    echo 'hello';
+
+// Traitement erreur et sécurité
+error_reporting(-1);
+ini_set('display_errors', 'On');
+set_error_handler("var_dump");
+define ('CONST_INCLUDE', NULL);
+
+require_once path(__DIR__ . '/../App/Connexion/Connexion.php');
+require_once path(__DIR__ . '/../App/Controller/DefaultController.php');
+
+use App\Connexion;
+use App\Controller\DefaultController;
+
+$connexion = new Connexion())::init();
+$controller = new Controller($connexion);
+$controller->redirect();
+
 ?>
