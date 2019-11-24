0e3c7812bb015149bd9c987890e156ecc57c35b5
Yukitsuo <uy.alexandre@yahoo.com>
2019-11-24 18:06:15 +0100

    CCP15 front(connection): make connection page

--- front/front/src/App.js
+++ front/front/src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Nav from './Nav';
 import Footer from './Footer';
+import Connection from './Connection';
 import logo from './logo.svg';
 import Lobby from './Lobby';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App container-fluid ml-lg-4">
       <Nav/>
-      <Lobby/>
-      <Footer/>
+      <Connection/>
     </div>
   );
 }
---
+++ front/front/src/Connection.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Divider from './Divider';
+
+
+class Connection extends React.Component {
+    render(){
+       return (
+        <section className="content row connection-bloc">
+            <div className="container">
+              <form className="col-lg-4 col-lg-offset-4 col-sm">
+                  <EmailAndPseudoInput/>
+                  <PasswordInput/>
+                  <ButtonConnection/>
+              </form>
+            </div>
+
+        </section>
+       )
+    }
+}
+
+class EmailAndPseudoInput extends React.Component {
+    render(){
+        return (
+            <div className="form-group">
+                <input type="email" className="form-control text-center mt-0 rounded-1" id="InputMailPseudo" aria-describedby="emailHelp" placeholder="Pseudo ou email"/>
+            </div> 
+
+        )
+    }
+}
+
+class PasswordInput extends React.Component {
+    render() {
+        return(   
+            <div className="form-group">
+                <input type="password" className="form-control text-center mt-0 rounded-1 custom" id="InputPassword" aria-describedby="emailHelp" placeholder="Mot de Passe"/>
+            </div>              
+        )
+    }
+}
+
+class ButtonConnection extends React.Component {
+    render(){
+        return(
+            <button type="button" class="btn btn-default btn-transparent mt-0 rounded-1 custom">Connexion </button>
+        )
+    }
+}
+
+export default Connection;
