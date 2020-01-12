97243b2a740afa95232c73521e7595551141b8eb
Djodjo <mathysimion@yahoo.fr>
2020-01-12 20:58:48 +0100

    CCP-34 fix/back(search): fix lobbies query

--- front/src/View/General/ProposalList.tsx
+++ front/src/View/General/ProposalList.tsx
@@ -27,10 +27,10 @@ class ProposalList extends React.Component<{ proposals: [] }, any> {
     public render(): ReactNode {
         return (
             <div
-                className={'proposal-section col-lg-7 col-sm-10 mt-sm-2 ml-lg-5 ml-sm-0 pt-sm-1 pl-lg-5 pl-sm-4 position-absolute'}
+                className={'proposal-section col-lg-7 col-sm-10 mt-5 mt-sm-2 ml-lg-5 ml-sm-0 pt-2 pt-sm-1 pr-5 pl-0 pl-lg-5 pl-sm-4 position-absolute'}
             >
                 <div
-                    className={'rounded proposal-list mt-sm-5 ml-lg-5 pt-2 pl-lg-2 pb-1'}
+                    className={'rounded proposal-list mt-sm-5 ml-lg-5 pt-2 pl-2 pl-lg-2 pb-1'}
                 >
                     <ul
                         className={'list-unstyled ml-lg-5 ml-sm-3'}
