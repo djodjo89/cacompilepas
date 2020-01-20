b7faa183f52cf247a9084c16e27eeb04176903e3
Djodjo <mathysimion@yahoo.fr>
2020-01-20 12:11:00 +0100

    CCP-69-8 fix/front(searchbar): hotfix input

--- cacompilepas/front/src/View/Search/ProposalList.tsx
+++ cacompilepas/front/src/View/Search/ProposalList.tsx
@@ -1,6 +1,6 @@
-import React, {ReactNode} from "react";
-import UserProposal from "./UserProposal";
-import LobbyProposal from "./LobbyProposal";
+import React, {ReactNode} from 'react';
+import UserProposal from './UserProposal';
+import LobbyProposal from './LobbyProposal';
 import '../../css/SearchBar.css';
 
 class ProposalList extends React.Component<{ proposals: any }, any> {
@@ -47,10 +47,10 @@ class ProposalList extends React.Component<{ proposals: any }, any> {
     public render(): ReactNode {
         return (
             <div
-                className={'proposal-section col-lg-9 col-md-8 col-sm-12 mt-5 mt-lg-2 mt-md-2 mt-sm-3 offset-0 offset-md-1 pt-2 pt-sm-1 pr-5 pr-lg-5 pr-md-0 pr-sm-0 pl-0 position-absolute'}
+                className={'proposal-section col-lg-9 col-md-8 col-sm-12 mt-5 offset-0 offset-md-1 pt-2 pr-5 pr-lg-5 pr-md-0 pr-sm-0 pl-0 position-absolute'}
             >
                 <div
-                    className={'rounded proposal-list w-100 mt-sm-5 pt-2 pr-lg-5 pl-0 pb-1'}
+                    className={'rounded proposal-list w-100 pt-2 pt-md-2 pt-sm-3 pr-lg-5 pl-0 pb-1'}
                 >
                     <ul
                         className={'list-unstyled ml-lg-5 ml-sm-0'}
